3776a36c5d8688ab8de3eae40d083e39eae9a1d6
Gregory Bakhtin <greasha46@gmail.com>
2025-06-26 16:00:47 +0300

    add parsing

--- lang.go
+++ lang.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	_ "path/filepath"
+	"strconv"
+	"strings"
 )
 
 // #cgo CFLAGS: -Ivendor
@@ -10,16 +12,23 @@ import (
 // #include "gale.h"
 // import "C"
 
+var program string = `crop 10% 20%
+> imgs/картинк*
+> scan/imgs/here/*
+
+rotate left
+rotate right`
+
 const (
-    ImgFormatNotSupported = iota
-    ImgFormatJpg
-    ImgFormatPng
-    ImgFormatBmp
-    ImgFormatTga
-    ImgFormatPsd
-    ImgFormatHdr
-    ImgFormatGif
-    ImgFormatPnm
+	ImgFormatNotSupported = iota
+	ImgFormatJpg
+	ImgFormatPng
+	ImgFormatBmp
+	ImgFormatTga
+	ImgFormatPsd
+	ImgFormatHdr
+	ImgFormatGif
+	ImgFormatPnm
 )
 
 type LexState int
@@ -40,72 +49,297 @@ const (
 
 type LexWord struct {
 	beg, end int
-	kind LexWordKind
+	kind     LexWordKind
 }
 
-type Lexer struct {
-	s LexState
-	words []LexWord
+func (w LexWord) String() string {
+	return strings.TrimSpace(program[w.beg:w.end])
 }
 
-var l Lexer
+type Words []LexWord
 
-const program string = `crop 10px 20px 30px 40px
-> imgs/картинк*
-> scan/imgs/here/*
+var words Words
 
-rotate left
-rotate right`
+func (w Words) iterator() func() (LexWord, bool) {
+	var i int
+	return func() (LexWord, bool) {
+		if i < len(w) {
+			i++
+			return w[i-1], true
+		}
+		return LexWord{}, false
+	}
+}
 
+type RuleKind int
+
+const (
+	NilRule RuleKind = iota
+	Crop
+	RotateLeft
+	RotateRight
+	FlipVertically
+	FlipHorizontally
+)
+
+type MeasurementUnit int
+
+const (
+	Pixel MeasurementUnit = iota
+	Percent
+)
+
+type CropParam struct {
+	value int
+	unit  MeasurementUnit
+}
+
+type CropParams struct {
+	top, right, bottom, left CropParam
+}
+
+type Rule struct {
+	kind   RuleKind
+	params interface{}
+}
+
+var generalRules []Rule
+
+type Img string
+
+type Block struct {
+	imgs  []Img
+	rules []Rule
+}
+
+var blocks []Block
 
 func main() {
 	lex()
+	parse()
+	exec()
+}
+
+func exec() {
+	for _, block := range blocks {
+		fmt.Println("exec", block)
+		block.applyRules()
+	}
+}
+
+func (b *Block) applyRules() {
+	for _, img := range b.imgs {
+		img.applyRules(generalRules)
+		img.applyRules(b.rules)
+	}
+}
+
+func (i Img) applyRules(rules []Rule) {
 }
 
 func lex() {
+	var s LexState
 	var w LexWord
 	for i, r := range program {
 		switch r {
 		case '\r', '\n':
-			if l.s == LexStateScanWord || l.s == LexStateScanFilepath {
+			if s == LexStateScanWord || s == LexStateScanFilepath {
 				w.end += 1
-				l.words = append(l.words, w)
+				words = append(words, w)
 			}
-			l.s = LexStateNil
-			if len(l.words) > 0 && l.words[len(l.words)-1].kind != LexNewLWord {
-				l.words = append(l.words, LexWord{kind: LexNewLWord})
+			s = LexStateNil
+			if len(words) > 0 && words[len(words)-1].kind != LexNewLWord {
+				words = append(words, LexWord{kind: LexNewLWord})
 			}
 		case ' ', '\t':
-			if l.s == LexStateScanWord {
+			if s == LexStateScanWord {
 				w.end += 1
-				l.words = append(l.words, w)
+				words = append(words, w)
 			}
-			if l.s == LexStateScanFilepath {
+			if s == LexStateScanFilepath {
 				w.end += 1
 				continue
 			}
-			l.s = LexStateNil
+			s = LexStateNil
 		case '>':
-			if l.s != LexStateNil {
+			if s != LexStateNil {
 				panic("error")
 			}
-			l.s = LexStateScanFilepath
-			w = LexWord {beg: i+1, end: i, kind: LexFilepathWord}
+			s = LexStateScanFilepath
+			w = LexWord{beg: i + 1, end: i, kind: LexFilepathWord}
 		default:
-			if l.s == LexStateScanWord || l.s == LexStateScanFilepath {
+			if s == LexStateScanWord || s == LexStateScanFilepath {
 				w.end += 1
 			} else {
-				w = LexWord {beg: i, end: i}
-				l.s = LexStateScanWord
+				w = LexWord{beg: i, end: i}
+				s = LexStateScanWord
 			}
 		}
 	}
-	if l.s == LexStateScanFilepath || l.s == LexStateScanWord {
+	if s == LexStateScanFilepath || s == LexStateScanWord {
 		w.end += 1
-		l.words = append(l.words, w)
+		words = append(words, w)
 	}
-	for _, word := range l.words {
-		fmt.Println(program[word.beg:word.end])
+	// for _, word := range words {
+	// 	fmt.Println(program[word.beg:word.end])
+	// }
+}
+
+func parse() {
+	var generalRulesWasScanned bool
+	var rule Rule
+	var block Block
+	iter := words.iterator()
+	for w, ok := iter(); ok; w, ok = iter() {
+		s := w.String()
+		switch w.kind {
+		case LexBaseWord:
+			switch s {
+			case "crop", "c":
+				rule = parseCrop(iter)
+			case "rotate":
+				if w, ok = iter(); !ok || w.kind != LexBaseWord {
+					panic("bad")
+				}
+				switch w.String() {
+				case "right":
+					rule = Rule{
+						kind: RotateRight,
+					}
+				case "left":
+					rule = Rule{
+						kind: RotateLeft,
+					}
+				default:
+					panic("bad")
+				}
+			case "flip":
+				if w, ok = iter(); !ok || w.kind != LexBaseWord {
+					panic("bad")
+				}
+				switch w.String() {
+				case "vertically":
+					rule = Rule{
+						kind: FlipVertically,
+					}
+				case "horizontally":
+					rule = Rule{
+						kind: FlipHorizontally,
+					}
+				default:
+					panic("bad")
+				}
+			case "l":
+				rule = Rule{kind: RotateLeft}
+			case "r":
+				rule = Rule{kind: RotateRight}
+			case "v":
+				rule = Rule{kind: FlipVertically}
+			case "h":
+				rule = Rule{kind: FlipHorizontally}
+			}
+		case LexFilepathWord:
+			generalRulesWasScanned = true
+			if len(block.rules) == 0 {
+				block.imgs = append(block.imgs, Img(s))
+			} else {
+				blocks = append(blocks, block)
+			}
+			continue
+		case LexNewLWord:
+			continue
+		}
+		if generalRulesWasScanned {
+			block.rules = append(block.rules, rule)
+		} else {
+			generalRules = append(generalRules, rule)
+		}
+	}
+	blocks = append(blocks, block)
+}
+
+func parseCrop(iter func() (LexWord, bool)) Rule {
+	var w LexWord
+	var ok bool
+	ws := make([]LexWord, 0, 5)
+	for w, ok = iter(); ok && w.kind == LexBaseWord; w, ok = iter() {
+		ws = append(ws, w)
+	}
+	if len(ws) == 0 || len(ws) == 3 || len(ws) >= 5 {
+	}
+	switch len(ws) {
+	case 1:
+		return Rule{
+			kind: Crop,
+			params: CropParams{
+				top:    parseCropParam(ws[0]),
+				right:  parseCropParam(ws[0]),
+				bottom: parseCropParam(ws[0]),
+				left:   parseCropParam(ws[0]),
+			},
+		}
+	case 2:
+		return Rule{
+			kind: Crop,
+			params: CropParams{
+				top:    parseCropParam(ws[0]),
+				right:  parseCropParam(ws[1]),
+				bottom: parseCropParam(ws[0]),
+				left:   parseCropParam(ws[1]),
+			},
+		}
+	case 4:
+		return Rule{
+			kind: Crop,
+			params: CropParams{
+				top:    parseCropParam(ws[0]),
+				right:  parseCropParam(ws[1]),
+				bottom: parseCropParam(ws[2]),
+				left:   parseCropParam(ws[3]),
+			},
+		}
+	case 0:
+		fallthrough
+	case 3:
+		fallthrough
+	default:
+		panic("wrong numbers for crop action, possible only 1, 2 or 4")
+	}
+	return Rule{}
+}
+
+func parseCropParam(w LexWord) CropParam {
+	var unit MeasurementUnit
+	var value int
+	var digitEndScanning int
+	s := w.String()
+	for digitEndScanning < len(s) {
+		c := s[digitEndScanning]
+		if !('0' <= c && c <= '9') {
+			break
+		}
+		digitEndScanning++
+	}
+	if digitEndScanning == 0 {
+		panic("bad")
+	}
+	value, err := strconv.Atoi(s[:digitEndScanning])
+	if err != nil {
+		panic("bad")
+	}
+	if digitEndScanning < len(s) {
+		switch s[digitEndScanning:] {
+		case "px":
+			unit = Pixel
+		case "%":
+			unit = Percent
+		default:
+			panic("bad")
+		}
+	}
+	return CropParam{
+		value: value,
+		unit:  unit,
 	}
 }
 
